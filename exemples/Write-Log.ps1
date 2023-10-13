3cf0236cd7e4a57a08ae85b878dcc7043ff877ea
Charles MANGENEY <contact@glm-system.fr>
2023-10-13 09:36:07 +0200

    Ajout fichier example Write-Log.ps1

---
+++ exemples/Write-Log.ps1
@@ -0,0 +1,42 @@
+﻿ param(
+        [Parameter(Mandatory = $true)]
+        $message,
+
+        [Parameter(Mandatory = $true)]
+        [ValidateSet("info", "warning", "error")]
+        $level,
+
+        [Parameter(Mandatory = $true)]
+        $path
+
+
+    )
+
+    # Définition de la couleur par niveau de message
+    $textcolor = "White"
+    
+    if($level -like "warning"){
+        $textcolor = "yellow"
+    }
+    elseif($level -like "error"){
+        $textcolor = "red"
+    }
+
+
+
+    Write-Verbose "Génération du timestamp"
+    # Récupération du timestamp
+    $timeStamp = Get-Date -Format "yyyy-MM-dd hh:mm:ss"
+    
+    # Construction de la chaîne du message
+    Write-Verbose "Construction de la chaîne du message"
+    ## Ecriture 1
+    $stringMsg = "{0} - {1} - {2}" -f $timeStamp, $level, $message
+    ## Ecriture 2
+    $stringMsg = "$timeStamp - $level - $message"
+
+
+    Write-Host $stringMsg -ForegroundColor $textcolor
+
+    Write-Verbose "Ecrite tu fichier $path avec argument Append"
+    $stringMsg | Out-File $path -Append
