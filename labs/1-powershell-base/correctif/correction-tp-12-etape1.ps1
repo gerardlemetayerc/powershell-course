96ac3a1714095804904c552dd721b92df0e73e45
Charles MANGENEY <contact@glm-system.fr>
2023-10-13 10:30:50 +0200

    Ajout de la correction - etape 1

---
+++ labs/1-powershell-base/correctif/correction-tp-12-etape1.ps1
@@ -0,0 +1,26 @@
+﻿$maximum = 30
+$minimum = 1
+$nbEssai = 7
+
+$perdu = $true
+
+$nbAleatoire = Get-Random -Minimum $minimum -Maximum $maximum
+clear
+do{
+    [int]$reponse = Read-Host "Entrez le nombre à deviner (entre $minimum et $maximum - nb essai restant : $nbEssai)"
+    if($reponse -gt $nbAleatoire){
+        Write-Host -ForegroundColor Yellow "Le chiffre est plus petit."
+    }elseif($reponse -lt $nbAleatoire)
+    {
+        Write-Host -ForegroundColor Yellow "Le chiffre est plus grand."
+    }else{
+        $perdu = $false
+        Write-Host -ForegroundColor Green "Bravo, vous avez trouvé."
+    }
+    $nbEssai--
+}while($nbEssai -gt 0 -and $nbAleatoire -ne $reponse)
+
+if($perdu)
+{
+    Write-Host -ForegroundColor Red "Désolé, vous avez perdu."
+}
